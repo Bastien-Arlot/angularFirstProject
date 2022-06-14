f280fd4e46c1939d5711caceb8d7af59ef08c6bc
Bastien Arlot <arlot.b@gmail.com>
2022-06-14 16:38:08 +0200

    add directive custom

--- ng-pokemon-app/src/app/border-card.directive.ts
+++ ng-pokemon-app/src/app/border-card.directive.ts
@@ -1,21 +1,28 @@
-import {Directive, ElementRef, HostListener} from '@angular/core';
+import {Directive, ElementRef, HostListener, Input} from '@angular/core';
 
 @Directive({
   selector: '[pkmnBorderCard]'
 })
 export class BorderCardDirective {
 
+  private initialColor: string = "#f5f5f5";
+  private defaultColor: string = "#009688";
+  private defaultHeight: number = 100;
+
   constructor(private el: ElementRef) {
-    this.setHeight(100);
-    this.setBorder('#f5f5f5');
+    this.setHeight(this.defaultHeight);
+    this.setBorder(this.initialColor);
   }
 
+  @Input('pkmnBorderCard') borderColor: string; //alias
+  // @Input() pkmnBorderCard: string; //sans alias
+
   @HostListener('mouseenter') onMouseEnter(){
-    this.setBorder('#009688');
+    this.setBorder(this.borderColor || this.defaultColor);
   }
 
   @HostListener('mouseleave') onMouseLeave(){
-    this.setBorder('#f5f5f5');
+    this.setBorder(this.initialColor);
   }
 
   setHeight(height: number){
