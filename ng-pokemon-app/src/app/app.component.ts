e934e80c3c6e385c68228b797f989306accafd29
Bastien Arlot <arlot.b@gmail.com>
2022-06-15 11:05:01 +0200

    add routing between details and list

--- ng-pokemon-app/src/app/app.component.ts
+++ ng-pokemon-app/src/app/app.component.ts
@@ -6,23 +6,5 @@ import {POKEMONS} from "./mock-pokemon-list";
     selector: 'app-root',
     templateUrl: 'app.component.html'
 })
-export class AppComponent implements OnInit {
-    pokemonList: Pokemon[] = POKEMONS;
-    pokemonSelected: Pokemon | undefined;
+export class AppComponent {}
 
-    ngOnInit() {
-        console.table(this.pokemonList);
-    }
-
-    selectPokement(pokemonId: string) {
-        const pokemon: Pokemon | undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
-        if (pokemon) {
-            console.log(`Vous avez cliqué sur le pokemon ${pokemon.name}`);
-            this.pokemonSelected = pokemon;
-        } else {
-            console.log("Vous avez demandé un pokemon qui n'existe pas");
-            this.pokemonSelected = pokemon;
-        }
-
-    }
-}
