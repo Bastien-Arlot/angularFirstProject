e7a7ff9f76f8feebad414caf1db2ac8a0c231fbb
Bastien Arlot <arlot.b@gmail.com>
2022-06-15 11:33:31 +0200

    add module pokemon to separate our code, adjusted the routing to have pokemon module before app routning

--- ng-pokemon-app/src/app/app.component.ts
+++ ng-pokemon-app/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {Pokemon} from "./pokemon";
-import {POKEMONS} from "./mock-pokemon-list";
+import {Pokemon} from "./pokemon/pokemon";
+import {POKEMONS} from "./pokemon/mock-pokemon-list";
 
 @Component({
     selector: 'app-root',
--- ng-pokemon-app/src/app/app.module.ts
+++ ng-pokemon-app/src/app/app.module.ts
@@ -3,24 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BorderCardDirective } from './border-card.directive';
-import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
-import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
-import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {PokemonModule} from "./pokemon/pokemon.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    BorderCardDirective,
-    PokemonTypeColorPipe,
-    ListPokemonComponent,
-    DetailPokemonComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    PokemonModule,
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
